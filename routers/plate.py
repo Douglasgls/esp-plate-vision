5ed926b4a60e617fe1af6ced95d5d6661f37e2b3
douglas <douglaspaz955@gmail.com>
2025-10-13 16:02:18 -0300

    feat:rota para salvar fotos na API

--- routers/plate.py
+++ routers/plate.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from urllib import request
+from fastapi import APIRouter, Request
 
 from fastapi import UploadFile, File
 from fastapi.responses import JSONResponse
@@ -6,6 +7,10 @@ from io import BytesIO
 import numpy as np
 import PIL.Image
 from uteis import validate_plate
+from PIL import Image
+import io
+from datetime import datetime
+
 
 router = APIRouter(
     prefix="/plate",
@@ -27,3 +32,23 @@ async def validate_plate_image(file: UploadFile = File(...)):
         content=plate
     )
 
+
+
+@router.post("/uploadfile")
+async def create_upload_file(request: Request):
+    try:
+        data = await request.body()
+
+        image_jpg = Image.open(io.BytesIO(data))
+
+        file_temp_now = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"uploads/camera_{file_temp_now}.png" 
+        image_jpg.save(filename, format="PNG")
+
+        return {"filename": filename}
+
+    except Image.UnidentifiedImageError:
+        return {"error": "Could not identify image file. Check file format/contents."}, 400
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return {"error": "An unexpected error occurred"}, 500
